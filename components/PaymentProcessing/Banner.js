96b94ba4918e67766e94312dee166be27dc9b81d
Andrew Quartey <yestrup@gmail.com>
2020-01-25 01:32:47 +0000

    Banner - desktop view

--- components/PaymentProcessing/Banner.js
+++ components/PaymentProcessing/Banner.js
@@ -7,7 +7,7 @@ class Banner extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-lg-6 col-md-12 p-0">
-                            <div className="payment-processing-banner-content">
+                            {/* <div className="payment-processing-banner-content">
                                 <h1>The new standard in online payments</h1>
                                 <p>
                                     It should be noted that although application software is thought
@@ -23,12 +23,36 @@ class Banner extends Component {
                                 <a href="#" className="btn btn-primary">
                                     Get Started!
                                 </a>
-                            </div>
+                            </div> */}
                         </div>
 
-                        <div className="col-lg-6 col-md-12 p-0">
-                            <div className="payment-processing-banner-image">
-                                <img src={require('../../images/payment-banner.jpg')} alt="image" />
+                        <div className="col-12 p-0">
+                            <div
+                                style={{
+                                    background: `url('../../images/banner.jpg')`,
+                                    backgroundSize: 'cover',
+                                    height: '650px'
+                                }}
+                            >
+                                {/* <div className="payment-processing-banner-image">
+                                <img src={require('../../images/banner.jpg')} alt="image" />
+                            </div> */}
+                                <div className="banner-text">
+                                    <h1 style={{ color: 'white' }}>
+                                        Empowering Ghana Through the SDGs
+                                    </h1>
+                                    <p
+                                        style={{
+                                            color: '#fef',
+                                            position: 'absolute',
+                                            left: '50%',
+                                            transform: `translate(-50%, -50%)`
+                                        }}
+                                    >
+                                        {' '}
+                                        The new standard in online payments
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
