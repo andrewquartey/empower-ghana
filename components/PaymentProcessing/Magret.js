5dcc6798ac64a69d3bb57b13605fd6c8a3667a45
Andrew Quartey <yestrup@gmail.com>
2020-02-26 02:30:29 +0000

    more img corrections

--- components/PaymentProcessing/Magret.js
+++ components/PaymentProcessing/Magret.js
@@ -16,7 +16,7 @@ class Gandhi extends Component {
                 <div className="container">
                     <div className="quote">
                         <div className="testimonial-quote group">
-                            <img src="/images/magret.png" />
+                            <img src={require('../../images/magret.png')} />
                             <div className="quote-container">
                                 <blockquote>
                                     <p>
